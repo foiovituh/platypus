322130be0e5663252b567047b0074d9920fcbc78
foiovituh <vitorsilvaalvescontato@gmail.com>
2022-10-19 00:45:07 -0300

    create README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+### platypus 🦆
+
+DNS toolbox
+
+### requirements 
+- python3
+- pip3
+
+### setup
+- pip3 install -r requirements.txt
+
+### usage
+```
+python3 platypus.py [module type]
+    MODULES: --sb (DNS subdomain bruteforce) [word list path]
+    EXAMPLE: python3 platypus.py --sb
+      host name: google.com
+      word list path: word_lists/example-10.txt
+```
--- modules/commons/messages.py
+++ modules/commons/messages.py
@@ -4,6 +4,6 @@ MISSING_ARGUMENTS = "missing arguments! use --h flag for help"
 HELP = "USAGE: python3 platypus.py [module type]\n" \
     "MODULES:\n  --sb (DNS subdomain bruteforce) [word list path]\n" \
     "EXAMPLE:\n  python3 platypus.py --sb\n" \
-    "  host name or IPV4: spotify.com\n" \
+    "  host name: google.com\n" \
     "  word list path: word_lists/example-10.txt\n" \
     "FOR MORE INFORMATION, SEE: https://github.com/foiovituh/platypus"
