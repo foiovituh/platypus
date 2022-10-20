4a6320b86881fe6175591667d3564bd491af81b7
foiovituh <vitorsilvaalvescontato@gmail.com>
2022-10-20 18:56:46 -0300

    deal with 'Invalid Arguments'

--- platypus.py
+++ platypus.py
@@ -1,4 +1,6 @@
-from modules.commons.messages import HELP, MISSING_ARGUMENTS
+from modules.commons.messages import (
+    HELP, INVALID_ARGUMENTS,MISSING_ARGUMENTS
+)
 from modules.commons.utils import print_and_exit
 from modules.dns import execute_subdomain_bruteforce
 from pyfiglet import print_figlet
@@ -14,6 +16,8 @@ def main():
             input("host name: "),
             input("word list path: "),
         )
+    else:
+        print_and_exit(INVALID_ARGUMENTS, 1)
 
 if __name__ == "__main__":
     print_figlet("platypus", "standard")
