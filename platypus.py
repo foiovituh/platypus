be2bb146a78a6ec3c8bf04a4a56754390f0b7cad
Vitu Ohto <68431603+foiovituh@users.noreply.github.com>
2022-10-20 19:50:32 -0300

    fix exit codes

--- platypus.py
+++ platypus.py
@@ -1,5 +1,7 @@
 from modules.commons.messages import (
-    HELP, INVALID_ARGUMENTS,MISSING_ARGUMENTS
+    HELP,
+    INVALID_ARGUMENTS,
+    MISSING_ARGUMENTS
 )
 from modules.commons.utils import print_and_exit
 from modules.dns import execute_subdomain_bruteforce
@@ -8,7 +10,7 @@ from sys import argv
 
 def main():
     if len(argv) == 1:
-        print_and_exit(MISSING_ARGUMENTS, 0)
+        print_and_exit(MISSING_ARGUMENTS, 1)
     elif argv[1] == "--h":
         print_and_exit(HELP, 0)
     elif argv[1] == "--sb":
